a1d0da307540599e15dd490154bec7c69542131b
YeahItsHayden <haydenparsons2@gmail.com>
2021-10-18 23:23:55 +1000

    f

--- core/player/cl_init.lua
+++ core/player/cl_init.lua
@@ -5,13 +5,3 @@ function SS.Player.GetCoords(entity)
     
     return vec
 end
-
-function SS.Player.RemoveProp()
-    for k, v in pairs(GetGamePool('CObject')) do
-        if IsEntityAttachedToEntity(PlayerPedId(), v) then
-            SetEntityAsMissionEntity(v, true, true)
-            DeleteObject(v)
-            DeleteEntity(v)
-        end
-    end
-end
---
+++ util/cl_util_functions.lua
@@ -0,0 +1,9 @@
+function SS.Function.RemoveProp()
+    for k, v in pairs(GetGamePool('CObject')) do
+        if IsEntityAttachedToEntity(PlayerPedId(), v) then
+            SetEntityAsMissionEntity(v, true, true)
+            DeleteObject(v)
+            DeleteEntity(v)
+        end
+    end
+end
