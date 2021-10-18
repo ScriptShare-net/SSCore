e2b0431fe7af459ac10d66b903dcb0caf584e147
YeahItsHayden <haydenparsons2@gmail.com>
2021-10-18 23:22:15 +1000

    add(client): Delete Prop function

--- core/player/cl_init.lua
+++ core/player/cl_init.lua
@@ -5,3 +5,13 @@ function SS.Player.GetCoords(entity)
     
     return vec
 end
+
+function SS.Player.RemoveProp()
+    for k, v in pairs(GetGamePool('CObject')) do
+        if IsEntityAttachedToEntity(PlayerPedId(), v) then
+            SetEntityAsMissionEntity(v, true, true)
+            DeleteObject(v)
+            DeleteEntity(v)
+        end
+    end
+end
