e23aaa6be05b02b4efb58b33177c49161146cb86
YeahItsHayden <haydenparsons2@gmail.com>
2021-10-18 23:16:00 +1000

    bug(server): Fix functions

--- util/sh_util_functions.lua
+++ util/sh_util_functions.lua
@@ -1,5 +1,5 @@
 function SS.GenerateRandomNumber(min, max, cb)
-    math.randomseed(os.time())
+    math.randomseed(os.time() * math.random())
     local number = math.random(min, max)
 
     cb(number)
