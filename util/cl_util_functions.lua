327e9004b40a3ca090c0dd87dcb5dbb53e01997c
YeahItsHayden <haydenparsons2@gmail.com>
2021-10-18 23:50:25 +1000

    add(util):Add Draw Text

--- util/cl_util_functions.lua
+++ util/cl_util_functions.lua
@@ -7,3 +7,18 @@ function SS.Function.RemoveProp()
         end
     end
 end
+
+function SS.Function.DrawText(coords, text)
+    SetTextScale(0.35, 0.35)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(true)
+    AddTextComponentString(text)
+    SetDrawOrigin(v, 0)
+    DrawText(0.0, 0.0)
+    local factor = (string.len(text)) / 370
+    DrawRect(0.0, 0.0+0.0125, 0.017+ factor, 0.03, 0, 0, 0, 0)
+    ClearDrawOrigin()
+end
