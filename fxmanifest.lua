4c9552ca9ec044cd8915c0775361dd7654522e61
YeahItsHayden <haydenparsons2@gmail.com>
2021-10-21 14:46:33 +1000

    kek

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'SSCore - A Framework built by Hayden#6789 and Jones#1913'
+version '1.00'
+author 'ScriptShare.net'
+
+shared_scripts {
+    '**/**/sh_*.lua'
+}
+
+server_scripts {
+    '**/**/sv_*.lua',
+}
+
+client_scripts {
+    '**/**/cl_*.lua'
+}
