4183cdd597d085fd28246fab3e18b282b74d500e
YeahItsHayden <haydenparsons2@gmail.com>
2021-10-21 14:36:03 +1000

    f

--- core/logs/sv_init.lua
+++ core/logs/sv_init.lua
@@ -5,18 +5,17 @@ function SS.Logs.CreateLog(message, source, source2, coords)
     local targetPlayer = source2
     local event = message
     local location = coords
-
     if event and location and player then
         if not targetPlayer then  
-            PerformHttpRequest(SS.Logs.DiscordWebhook, function(err, text, headers) end, 'POST', json.encode({
-                username = Config.username, 
+            PerformHttpRequest(SS.Logs.DiscordWebhook, function(err, text, head) end, 'POST', json.encode({
+                username = "ScriptShare", 
                 embeds = {{
                     ["color"] = 255,255,255, 
                     ["author"] = {
                         ["name"] = SS.Logs.CommunityName,
                         ["icon_url"] = SS.Logs.IconURL
                     },
-                    ["title"] = GetTitle(channel),
+                    ["title"] = "Test",
                     ["description"] = "".. message .."",
                     ["footer"] = {
                         ["text"] = SS.Logs.FooterText.." • "..os.date("%x %X %p"),
@@ -27,9 +26,6 @@ function SS.Logs.CreateLog(message, source, source2, coords)
             }), { 
                 ['Content-Type'] = 'application/json' 
             })
-        else
-           
         end 
     end 
-
 end
