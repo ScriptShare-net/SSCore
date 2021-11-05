6b2e3eda186544457ebc8642f8cc5121c8c85483
YeahItsHayden <haydenparsons2@gmail.com>
2021-11-05 17:53:54 +1000

    Function to check if table contains element

--- util/func/sh_util_functions.lua
+++ util/func/sh_util_functions.lua
@@ -23,3 +23,12 @@ function SS.Entity.GetCoords(entity)
     
     return vec
 end
+
+function table.contains(table, element)
+    for _, value in pairs(table) do
+      if value == element then
+        return true
+      end
+    end
+    return false
+end
