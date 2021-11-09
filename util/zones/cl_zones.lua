8bcf112922196d8278868f76a0b70a59eefb92fb
Jones <36283056+Solositeous@users.noreply.github.com>
2021-11-09 14:27:27 +1000

    base code

--- util/zones/cl_zones.lua
+++
@@ -1,4 +0,0 @@
-RegisterNetEvent("SS:Client:CreateZone", function(coords)
-	local coords = {x = coords.x, y = coords.y, z = coords.z, size = 0.5}
-	exports["SSCore"]:blipsCreate(json.encode(coords), coords, {r = 255, g = 0, b = 0}, "", {}, function() end)
-end)
--- util/zones/sv_zones.lua
+++ util/zones/sv_zones.lua
@@ -6,7 +6,6 @@ SS.Players = SS.Players or {}
 
 local function initiate()
 	if not zoneCreated then return end
-	print("initiated")
 	CreateThread(function()
 		while zoneCreated do
 			Wait(100)
@@ -34,29 +33,6 @@ local function initiate()
 			end
 		end
 	end)
-end
-
-local function pointInPolygon( x, y, pnt)
-	--[[ local vertices = pnt ]]
-	local points = pnt
-		
-	--[[ for i=1, #vertices-1, 2 do
-		points[#points+1] = { x=vertices[i], y=vertices[i+1] }
-	end ]]
-
-	local i, j = #points, #points
-	local inside = false
-
-	for i=1, #points do
-		if ((points[i].y < y and points[j].y>=y or points[j].y< y and points[i].y>=y) and (points[i].x<=x or points[j].x<=x)) then
-			if (points[i].x+(y-points[i].y)/(points[j].y-points[i].y)*(points[j].x-points[i].x)<x) then
-				inside = not inside
-			end
-		end
-		j = i
-	end
-
-	return inside
 end
 
 exports("zoneCreate", function(identifier, points, minZ, maxZ, enterCallback, leaveCallback)
@@ -112,58 +88,21 @@ exports("zoneCreate", function(identifier, points, minZ, maxZ, enterCallback, le
 	end
 
 	self.isInside = function(coords)
-		return pointInPolygon(coords.x, coords.y, self.points)
-		--[[ local x = coords.x
-		local y = coords.y
-		local z = coords.z
+		local x, y, points = coords.x, coords.y, self.points
+		local i, j = #points, #points
+		local inside = false
 
-		if x < self.minX or x > self.maxX or y < self.minY or y > self.maxY then
-			return false
-		end
-
-		if z < self.minZ or z > self.maxZ then
-			return false
-		end
-
-		local oddNodes = false
-		local j = #self.points
-		local maxPoint = self.points[j]
-		for i, point in pairs(self.points) do
-			if (point.y < coords.y and maxPoint.y >= coords.y or maxPoint.y < coords.y and point.y >= coords.y) then
-				if (point.x + (coords.y - point.y) / (maxPoint.y - point.y) * (maxPoint.x - point.x) < coords.x) then
-					oddNodes = true
+		for i = 1, #points do
+			if (points[i].y < y and points[j].y > = y or points[j].y < y and points[i].y > = y) and (points[i].x < = x or points[j].x < = x) then
+				if points[i].x + (y - points[i].y) / (points[j].y - points[i].y) * (points[j].x - points[i].x) < x then
+					inside = not inside
 				end
 			end
-			j = i;
+			j = i
 		end
-		return oddNodes  ]]
+
+		return inside
 	end
 
 	zones[identifier] = self
-end)
-
-local newpoints = {}
-local newminZ = -100
-local newmaxZ = 100
-
-RegisterCommand("newpoint", function(source, args)
-	local coords = GetEntityCoords(GetPlayerPed(source))
-	newpoints[#newpoints + 1] = coords
-	if coords.z - 10 < newminZ then
-		newminZ = coords.z - 10
-	end
-
-	if coords.z + 10 > newmaxZ then
-		newmaxZ = coords.z + 10
-	end
-	print(json.encode(coords))
-	TriggerClientEvent("SS:Client:CreateZone", source, coords)
-end)
-
-RegisterCommand("createzone", function()
-	exports["SSCore"]:zoneCreate("testZone", newpoints, newminZ, newmaxZ, function()
-		print("Entered Zone")
-	end, function()
-		print("Left Zone")
-	end)
 end)
