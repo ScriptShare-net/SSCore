ddc8d5791b5f0389aafe69099b39ccb0400bcae6
YeahItsHayden <haydenparsons2@gmail.com>
2022-01-14 14:55:44 +1000

    add(multiChar): vec4 conversions for lua 5.4

--- modules/character_selector/cl_config.lua
+++ modules/character_selector/cl_config.lua
@@ -2,6 +2,6 @@ local SSCore = exports["SSCore"]
 
 --Spawns
 SSCore:SetConfigValue("CharacterSelectorSpawns", {
-	["Airport"] = vector4(-1030.08, -2731.66, 13.76, 15.86),
-	["Government Building"] = vector4(-519.91, -261.92, 35.51, 233.08),
+	["Airport"] = vec4(-1030.08, -2731.66, 13.76, 15.86),
+	["Government Building"] = vec4(-519.91, -261.92, 35.51, 233.08),
 })
