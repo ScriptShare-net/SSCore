b196845dcbb8524a77ffa190af7f7656956169f8
Jones <36283056+Solositeous@users.noreply.github.com>
2022-02-08 09:53:34 +1000

    moved teleport into cl_functions

--- core/cl_functions.lua
+++ core/cl_functions.lua
@@ -31,3 +31,13 @@ end)
 RegisterNetEvent("SS:Client:CharacterLoaded", function(characterdata)
 	SSCore:SetCharacterData(characterdata)
 end)
+
+exports("teleportPlayer", function(player, targetCoords)
+    if not player or not targetCoords then return end
+
+    if type(targetCoords) == 'table' then 
+        SetEntityCoords(player, targetCoords, 0, 0, 1)
+    else
+        SetEntityCoords(player, targetCoords.x, targetCoords.y, targetCoords.z, 0, 0, 1)
+    end
+end)
